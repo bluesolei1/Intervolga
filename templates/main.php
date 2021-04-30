5f53177d3b089d7a6cdd2c2d32b0f73f0fe2dd9a
unknown <bluesolei@mail.ru>
2021-04-30 21:49:05 +0300

    header, footer

--- templates/main.php
+++ templates/main.php
@@ -1,6 +1,4 @@
-<link href="../country/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-<script src="../country/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="../country/vendor/jquery-3.6.0.min.js"></script>
+<?php include_once __DIR__ ."./inc/header.php" ?>
 <div class="container">
 	<table class="table table-hover">
 		<thead>
@@ -21,43 +19,6 @@
 				<?php endforeach; ?>
 			</tr>	
 		</tbody>
-		
 	</table>
-	<form class="row row-cols-lg-auto g-3 align-items-center " id="edit" action="../country/index.php" method="POST" name="countryForm">
-		<div class="col-12">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Страна" name ="name">
-			</div>
-		</div>
-		
-		<div class="col-12">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Столица" name="capital">
-			</div>
-		</div>
-		
-		<div class="col-12">
-			<div class="input-group">
-				<input type="text" class="form-control" placeholder="Население" name="population">
-			</div>
-		</div>
-		
-		
-		
-		<div class="col-12"><button type="submit" class="btn btn-primary">Отправить</button>	</div>
-	</form>
 	
-	<button type="button" class="btn btn-primary" id="addCountry">Добавить</button>
-</div>
-
-
-
-<script>
-	$( document ).ready(function() {
-		$("#edit").hide();
-		$( "#addCountry" ).click(function() {
-			$("#edit").slideDown();
-			$(this).hide();
-		});
-	});
-</script>
+<?php include_once __DIR__ ."./inc/footer.php" ?>
